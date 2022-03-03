5b5aa3a702e4852032ac5fd83d5531d3f715a88a
Eksujpin <57792426+Eksujpin@users.noreply.github.com>
2022-03-03 12:54:10 +0100

    current most stable, dont work with 4x board
    Co-authored-by: Day0r <Day0r@users.noreply.github.com>
    Co-authored-by: Cromski <Cromski@users.noreply.github.com>
    Co-authored-by: Bjørnar Haugstad Jåtten <bjornarjatten@users.noreply.github.com>

--- project01-Othello/Minimax.java
+++ project01-Othello/Minimax.java
@@ -3,15 +3,17 @@
 public class Minimax implements IOthelloAI {
 
     public Position decideMove(GameState s){
+        Position firstLegal = s.legalMoves().get(0);
         Position movePosition = minimaxSearch(s);
-        System.out.println("Trying this move:");
-        System.out.println(movePosition);
+        System.out.println("Trying this move: "+ movePosition);
+        System.out.println("this is first move in legal: " + firstLegal);
+        System.out.println(s.legalMoves().contains(movePosition));
         return movePosition;
     }
 
     public Position minimaxSearch(GameState s){
         Pair doMove = maxValue(s);
-        System.out.println("found this move: "+doMove.pos);
+        System.out.println("found this move: "+ doMove.pos);
         if (doMove.pos == null) return new Position(-1,-1);
         else return doMove.pos;
     }
@@ -19,13 +21,14 @@ public class Minimax implements IOthelloAI {
     public Pair maxValue(GameState s){
         Pair curr = new Pair(0, null);
 
-        if(s.isFinished() == true)return curr;
+        if(s.isFinished() == true) return curr;
 
         for (var a : s.legalMoves()){
-            curr = new Pair(findCapScore(s, a), a); //im not sure about this
+            curr = new Pair(findCapScore(s, a), a);
+            s.insertToken(a);
             Pair min = minValue(s);
-            if(min.score > curr.score){ //im not sure about this
-                curr = min; //im not sure about this
+            if(min.score > curr.score){
+                curr = min; 
             }
         }
         return curr;
@@ -33,14 +36,14 @@ public class Minimax implements IOthelloAI {
 
     public Pair minValue(GameState s){
         Pair curr = new Pair(0, null);
-        if(s.isFinished() == true){
-            return curr;
-        }
+        if(s.isFinished() == true) return curr;
+
         for (var a : s.legalMoves()){
-            curr = new Pair(findCapScore(s, a), a);//im not sure about this
+            curr = new Pair(findCapScore(s, a), a);
+            s.insertToken(a);
             Pair max = maxValue(s);
-            if(max.score < curr.score){ //im not sure about this
-                curr = max; //im not sure about this
+            if(max.score < curr.score){
+                curr = max; 
             }
         }
         return curr;
