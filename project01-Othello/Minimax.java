a4776f471993b8648205469acc78d66d98827d1b
Bjørnar Haugstad Jåtten <bjja@itu.dk>
2022-03-10 12:27:38 +0100

    Added alpha-beta pruning

--- project01-Othello/Minimax.java
+++ project01-Othello/Minimax.java
@@ -1,35 +1,49 @@
 
 
 public class Minimax implements IOthelloAI {
+// int to state what side is using the minimax
 private int player;
 
+private int depth = 5;
+
+    /** 
+    */
     public Position decideMove(GameState s){
         player = s.getPlayerInTurn();
         Position pos = s.legalMoves().get(0);
-        Position movePosition = maxValue(s, pos).pos;
+        int step = 0;
+        Position movePosition = maxValue(s, pos, Integer.MIN_VALUE, Integer.MAX_VALUE, step).pos;
         return movePosition;
     }
 
-    public Pair maxValue(GameState s, Position pos){
-        if(s.isFinished()) return new Pair(findScore(s), pos);
+    public Pair maxValue(GameState s, Position pos, int alpha, int beta, int step){
+        if(s.isFinished()|| step >= depth) return new Pair(findScore(s), pos);
         Pair curr  = new Pair(findScore(s), pos);
         for (Position p : s.legalMoves()){
-            Pair v2a2 = minValue(result(s, p), curr.pos);
+            Pair v2a2 = minValue(result(s, p), curr.pos, alpha, beta, step+1);
             if (v2a2.score > curr.score){
                 curr = new Pair(v2a2.score, p);
             }
+            if (curr.score >= beta){
+                return curr;
+            }
+            alpha = Math.max(alpha, curr.score);
         } 
         return curr;
     }
 
-    public Pair minValue(GameState s, Position pos){
-        if(s.isFinished()) return new Pair(findScore(s), pos);
+    public Pair minValue(GameState s, Position pos, int alpha, int beta, int step){
+        if(s.isFinished() || step >= depth) return new Pair(findScore(s), pos);
         Pair curr  = new Pair(findScore(s), pos);
         for (Position p : s.legalMoves()){
-            Pair v2a2 = maxValue(result(s, p), curr.pos);
+            Pair v2a2 = maxValue(result(s, p), curr.pos, alpha, beta, step+1);
             if (v2a2.score < curr.score){
                 curr = new Pair(v2a2.score, p);
             }
+            if (curr.score <= alpha){
+                return curr;
+            }
+            beta = Math.max(beta, curr.score);
         } 
         return curr;
     }
