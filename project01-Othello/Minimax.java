2ae9d4d5e267b606ca836624dae05a5010c999a8
Eksujpin <57792426+Eksujpin@users.noreply.github.com>
2022-03-10 13:24:07 +0100

    does not work when moving first, something is deeply flawed

--- project01-Othello/Minimax.java
+++ project01-Othello/Minimax.java
@@ -3,10 +3,13 @@
 public class Minimax implements IOthelloAI {
 // int to state what side is using the minimax
 private int player;
-
+// the limit to what depth the ai is allowed to look forward
 private int depth = 5;
 
-    /** 
+    /**sets what player is playing with minimax,
+     * then defaults to the first legal move being the best until anouther move is found
+     * initializez the start depth to be 0
+     * then begins looking for the best move by calling maxvalue
     */
     public Position decideMove(GameState s){
         player = s.getPlayerInTurn();
@@ -16,9 +19,17 @@ private int depth = 5;
         return movePosition;
     }
 
+    /** Mimics the pseudocode from the book
+     * creates a new pair based on the current move
+     * then checks if we have reached our cutoff or the game is finished, if so returns the current move
+     * otherwise we go trough the list of legal moves
+     * calling minvalue on each move and procceding through the game, returning the best score at when a cutoff is reached
+     * this score is then compared to the current moves score and the best move is selected as the new current move
+     * 
+     */
     public Pair maxValue(GameState s, Position pos, int alpha, int beta, int step){
-        if(s.isFinished()|| step >= depth) return new Pair(findScore(s), pos);
         Pair curr  = new Pair(findScore(s), pos);
+        if(s.isFinished()|| step >= depth) return curr;
         for (Position p : s.legalMoves()){
             Pair v2a2 = minValue(result(s, p), curr.pos, alpha, beta, step+1);
             if (v2a2.score > curr.score){
@@ -27,14 +38,15 @@ private int depth = 5;
             if (curr.score >= beta){
                 return curr;
             }
+            
             alpha = Math.max(alpha, curr.score);
         } 
         return curr;
     }
 
     public Pair minValue(GameState s, Position pos, int alpha, int beta, int step){
-        if(s.isFinished() || step >= depth) return new Pair(findScore(s), pos);
         Pair curr  = new Pair(findScore(s), pos);
+        if(s.isFinished()|| step >= depth) return curr;
         for (Position p : s.legalMoves()){
             Pair v2a2 = maxValue(result(s, p), curr.pos, alpha, beta, step+1);
             if (v2a2.score < curr.score){
@@ -43,6 +55,7 @@ private int depth = 5;
             if (curr.score <= alpha){
                 return curr;
             }
+    
             beta = Math.max(beta, curr.score);
         } 
         return curr;
