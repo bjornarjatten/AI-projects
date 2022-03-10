cef33963f8a0edd8dc5c75d0ac9f064dae7e73de
Eksujpin <jonas@pj-computer.dk>
2022-03-10 15:23:00 +0100

    still dont work

--- project01-Othello/Minimax.java
+++ project01-Othello/Minimax.java
@@ -1,4 +1,4 @@
-
+import java.applet.Applet;
 
 public class Minimax implements IOthelloAI {
 // int to state what side is using the minimax
@@ -13,10 +13,11 @@ private int depth = 5;
     */
     public Position decideMove(GameState s){
         player = s.getPlayerInTurn();
-        Position pos = s.legalMoves().get(0);
         int step = 0;
-        Position movePosition = maxValue(s, pos, Integer.MIN_VALUE, Integer.MAX_VALUE, step).pos;
-        return movePosition;
+        Position temp =  s.legalMoves().get(0);
+        System.out.println(temp);
+        Pair movePosition = maxValue(s, Integer.MIN_VALUE, Integer.MAX_VALUE, step);
+        return movePosition.pos;
     }
 
     /** Mimics the pseudocode from the book
@@ -25,48 +26,42 @@ private int depth = 5;
      * otherwise we go trough the list of legal moves
      * calling minvalue on each move and procceding through the game, returning the best score at when a cutoff is reached
      * this score is then compared to the current moves score and the best move is selected as the new current move
-     * 
      */
-    public Pair maxValue(GameState s, Position pos, int alpha, int beta, int step){
-        Pair curr  = new Pair(findScore(s), pos);
-        if(s.isFinished()|| step >= depth) return curr;
-        for (Position p : s.legalMoves()){
-            Pair v2a2 = minValue(result(s, p), curr.pos, alpha, beta, step+1);
-            if (v2a2.score > curr.score){
-                curr = new Pair(v2a2.score, p);
+    public Pair maxValue(GameState s, int alpha, int beta, int step){
+        if(s.isFinished()|| step >= depth) return new Pair(Integer.MIN_VALUE, null);
+        Pair curPair = new Pair(Integer.MIN_VALUE, null);
+        for(Position a : s.legalMoves()){
+            Pair v2a2 = minValue( result(s, a), alpha, beta, step+1);
+            if(v2a2.score > findScore(s) ){
+                System.out.println("found better option");
+                curPair = new Pair(v2a2.score, a);
+                alpha = Math.max(alpha, curPair.score);
             }
-            if (curr.score >= beta){
-                return curr;
-            }
-            
-            alpha = Math.max(alpha, curr.score);
-        } 
-        return curr;
+            if (curPair.score >= beta) return curPair;
+        }
+        return curPair;
     }
 
-    public Pair minValue(GameState s, Position pos, int alpha, int beta, int step){
-        Pair curr  = new Pair(findScore(s), pos);
-        if(s.isFinished()|| step >= depth) return curr;
-        for (Position p : s.legalMoves()){
-            Pair v2a2 = maxValue(result(s, p), curr.pos, alpha, beta, step+1);
-            if (v2a2.score < curr.score){
-                curr = new Pair(v2a2.score, p);
+    public Pair minValue(GameState s, int alpha, int beta, int step){
+        if(s.isFinished()|| step >= depth) return new Pair(Integer.MAX_VALUE, null);
+        Pair curPair = new Pair(Integer.MAX_VALUE, null);
+        for(Position a : s.legalMoves()){
+            Pair v2a2 = maxValue( result(s, a), alpha, beta, step+1);
+            if(v2a2.score < findScore(s) ){
+                curPair = new Pair(v2a2.score, a);
+                beta = Math.min(beta, curPair.score);
             }
-            if (curr.score <= alpha){
-                return curr;
-            }
-    
-            beta = Math.max(beta, curr.score);
-        } 
-        return curr;
+            if (curPair.score <= alpha) return curPair;
+        }
+        return curPair;
     }
 
 
     //find the socre of the current game state
     private int findScore(GameState s){
         int[] Scores = s.countTokens();
-        if(player == 1) return Scores[0] - Scores[1];
-        else return Scores[1] - Scores[0];
+        if(player == 1) return Scores[0];
+        else return Scores[1];
     }
 
     private GameState result(GameState s, Position p){
