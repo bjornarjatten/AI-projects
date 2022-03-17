26b2f0b13b67dad6ba4f16352ea74dbdfdd69cc8
Eksujpin <57792426+Eksujpin@users.noreply.github.com>
2022-03-17 12:36:29 +0100

    coments in pair

--- project01-Othello/Pair.java
+++ project01-Othello/Pair.java
@@ -2,6 +2,9 @@ public class Pair {
     int score;
     Position pos;
 
+    /**
+     * Basic class for handeling a pair of (score,position)
+     */
     public Pair(int score, Position pos){
         this.score = score;
         this.pos = pos;
