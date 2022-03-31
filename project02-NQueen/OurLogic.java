f2d29db1747f3fd9062ceef0475e62641a5b787f
Eksujpin <jonas@pj-computer.dk>
2022-03-31 15:44:10 +0200

    it runs well now, left comment on how to compile, some refactoring of x,y or collum,row might be nice for readability, comments will arrive soonTM

--- project02-NQueen/OurLogic.java
+++ project02-NQueen/OurLogic.java
@@ -3,17 +3,18 @@ import net.sf.javabdd.*;
 public class OurLogic implements IQueensLogic{
     private int size;		// Size of quadratic game board (i.e. size = #rows = #columns)
     private int[][] board;	// Content of the board. Possible values: 0 (empty), 1 (queen), -1 (no queen allowed)
-    
     private BDDFactory fac;
-    private BDD yes;
-    private BDD no;
+    private BDD True;
+    private BDD False;
     private BDD bdd;
+
+    //TO COMPILE US THIS:  javac -cp .\javabdd-1.0b2.jar *.java in vscode
 
     public void initializeBoard(int size) {
         this.size = size;
+        System.out.println("this is size " + size);
         this.board = new int[size][size];
         buildBDD();
-        updateboard();
     }
    
     public int[][] getBoard() {
@@ -23,44 +24,65 @@ public class OurLogic implements IQueensLogic{
     public void buildBDD(){
         this.fac = JFactory.init(2000000, 200000);
         this.fac.setVarNum(this.size*this.size); 
-        this.no = this.fac.zero();
-        this.yes = this.fac.one();
-        this.bdd = yes;
-
-        for(int x = 0; x < size; x++){
-            for(int y = 0; y < size; y++){
-                eval(y,x);
-            }
-        }
+        this.False = this.fac.zero();
+        this.True = this.fac.one();
+        this.bdd = True;
+        rules();
         eightRule();
+        System.out.println("is ready");
     }
 
     private void eightRule(){
-        for(int x = 0; x < size; x++){
-            BDD subBdd = no;
-            for(int y = 0; y < size; y++){
-                subBdd = subBdd.or(this.fac.ithVar(place(y, x)));
+        for(int y = 0; y < size; y++){
+            BDD subBdd = False;
+            for(int x = 0; x < size; x++){
+                subBdd = subBdd.or(this.fac.ithVar(place(x, y)));
             }
             this.bdd = this.bdd.and(subBdd);
         }
     }
 
-
+    private void rules(){
+        for(int x = 0; x < size; x++){
+            for(int y = 0; y < size; y++){
+                eval(x,y);
+            }
+        }
+    }
     
 
-    private void eval(int y, int x){
-        BDD unavailableBDD = no;
-        BDD availableBDD = yes;      
+    private void eval(int x, int y){
+        BDD unavailableBDD = False;
+        BDD availableBDD = True;      
         
         //all y = false
         for(int y2 = 0; y2 < size; y2++){
-            if (y != y2) availableBDD = availableBDD.and(this.fac.nithVar(place(y2,x)));
+            if (y != y2) availableBDD = availableBDD.and(this.fac.nithVar(place(x,y2)));
         }
 
         //all x = false
         for(int x2 = 0; x2 < size; x2++){
-            if (x != x2) availableBDD = availableBDD.and(this.fac.nithVar(place(y,x2)));
+            if (x != x2) availableBDD = availableBDD.and(this.fac.nithVar(place(x2,y)));
         }
+    
+        // all diagonal left = false 
+        for (int x2 = 0; x2 < size; x2++) {
+            if (x != x2) {
+                if ((y+x2-x < size) && (y+x2-x > 0)) {
+                    availableBDD = availableBDD.and(this.fac.nithVar(place(x2,y+x2-x)));
+                }
+            }
+        }
+
+        // all diagonal right = false 
+        for (int x2 = 0; x2 < size; x2++) {
+            if (x != x2) {
+                if ((y-x2+x < size) && (y-x2+x > 0)) {
+                    availableBDD = availableBDD.and(this.fac.nithVar(place(x2,y-x2+x)));
+                }
+            }
+        }
+
 
         unavailableBDD = unavailableBDD.or(this.fac.nithVar(place(x,y)));
         unavailableBDD = unavailableBDD.or(availableBDD);
@@ -69,21 +91,21 @@ public class OurLogic implements IQueensLogic{
 
     }
     
+    // returns a variable number for a collum*row in the BDD
+    private int place(int column,int row) {
+        return row*this.size+column;
+      }
 
-    private int place(int y,int x) {
-      return x*this.size+y;
-    }
-
-    private boolean isInvalid(int y, int x){
-        BDD testBDD = this.bdd.restrict(this.fac.ithVar(place(y,x)));
+    private boolean isInvalid(int column, int row){
+        BDD testBDD = this.bdd.restrict(this.fac.ithVar(place(column,row)));
         return testBDD.isZero();
     }
     
     private void updateboard(){
         for(int i = 0; i < size; i++){
-            for(int j = 0; i < size; j++){
-                if(isInvalid(j, i)){
-                    board[j][i] = -1;
+            for(int j = 0; j < size; j++){
+                if(isInvalid(i, j)){
+                    board[i][j] = -1;
                 }
             }
         }
@@ -91,7 +113,7 @@ public class OurLogic implements IQueensLogic{
 
     public void insertQueen(int column, int row) {
         board[column][row] = 1;
+        this.bdd = this.bdd.restrict(this.fac.ithVar(row*this.size+column));
         updateboard();
     }    
-    
 }
